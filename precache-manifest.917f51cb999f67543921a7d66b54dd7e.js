141b2b929ab3a2fa2e6380035c2fc64711660268
sharafi <yalda.sharafi@gmail.com>
2020-03-19 00:48:27 +0330

    Updates

---
+++ precache-manifest.917f51cb999f67543921a7d66b54dd7e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8b2279dc199042f9f702b204d274324",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "55fab83febbe1abd31fe",
+    "url": "/monsters-rolodex/static/css/main.54106f90.chunk.css"
+  },
+  {
+    "revision": "7136eb73ea9c81d0121c",
+    "url": "/monsters-rolodex/static/js/2.cc8af93d.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters-rolodex/static/js/2.cc8af93d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55fab83febbe1abd31fe",
+    "url": "/monsters-rolodex/static/js/main.012c6e5d.chunk.js"
+  },
+  {
+    "revision": "bf0efc50803a2c0daead",
+    "url": "/monsters-rolodex/static/js/runtime-main.e47bae1e.js"
+  }
+]);
